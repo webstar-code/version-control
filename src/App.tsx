ab0c99a57aaa39b890d7f7b4536fe16886359325
webstar-code <bhaveshrc2001@gmail.com>
2023-09-05 23:22:18 +0530

    eslint fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function App () {
   return (
     <div className="App">
       <div className='flex justify-center items-center'>
