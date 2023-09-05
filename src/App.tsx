6ae7995c4ec7e1bcc38ba4e62cbf0477706876d6
webstar-code <bhaveshrc2001@gmail.com>
2023-09-05 23:01:33 +0530

    add firebase

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
-function App () {
+function App() {
   const [count, setCount] = useState(0)
 
   return (
@@ -24,18 +24,7 @@ function App () {
           <img src="/eslint.svg" className="logo" alt="ESLint logo" />
         </a>
       </div>
-      <h1>Vite + React 18 + TS + TailwindCSS + ESLint</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the logos to learn more
-      </p>
+      <h1>Vite + React 18 + TS + TailwindCSS + ESLint @v1</h1>
     </div>
   )
 }
