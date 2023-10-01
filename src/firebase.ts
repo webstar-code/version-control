3eb7c4d40b1781db7a17656b33956dcf69b5aa65
webstar-code <bhaveshrc2001@gmail.com>
2023-10-01 21:01:30 +0530

    add workflow for dev

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,17 +1,17 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAjZarffOW983ma2TRoNS1nGfvrlqQqf0c",
-  authDomain: "version-control-91d8f.firebaseapp.com",
-  projectId: "version-control-91d8f",
-  storageBucket: "version-control-91d8f.appspot.com",
-  messagingSenderId: "362941878238",
-  appId: "1:362941878238:web:4761b7a66f6207fbdb574f"
-};
+  apiKey: 'AIzaSyAjZarffOW983ma2TRoNS1nGfvrlqQqf0c',
+  authDomain: 'version-control-91d8f.firebaseapp.com',
+  projectId: 'version-control-91d8f',
+  storageBucket: 'version-control-91d8f.appspot.com',
+  messagingSenderId: '362941878238',
+  appId: '1:362941878238:web:4761b7a66f6207fbdb574f'
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig)
